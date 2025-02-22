de4f7afd01d6aae78668bf4a177d8682b448d397
ce335805 <eckhardt.cj@gmail.com>
2025-02-22 08:39:07 +0100

    import test-image from local npm link

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-<h1>
-  {{message}}
-</h1>
+  <h1>
+    {{ message }}
+  </h1>
+  <TestImageComponent/>
 </template>
 
 <script setup lang="ts">
-  const message: String = "This consumes my image component"
+import {TestImageComponent} from '@ce335805/test-image-component'
+
+const message: String = "This consumes my image component"
 </script>
